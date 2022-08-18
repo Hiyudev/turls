d2af27c7cf82bc2362e2e7674354bb5fd19978ff
Kevin <37001732+Hiyudev@users.noreply.github.com>
2022-08-17 23:52:13 -0300

    chore: revamp configurations

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ["cjs", "esm"],
-  splitting: false,
-  sourcemap: true,
+  format: 'esm',
   clean: true,
   minify: true,
   dts: true,
